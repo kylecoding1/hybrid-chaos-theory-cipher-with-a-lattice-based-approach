c5301e38d446cc33689778eeba673a234144759e
kylecoding1 <128002901+kylecoding1@users.noreply.github.com>
2023-08-21 15:17:26 -0700

    was able to raise entropy and keep decryption true

--- chaos.py
+++ chaos.py
@@ -5,15 +5,21 @@ from numpy.random import Generator, PCG64
 import secrets
 from typing import Tuple
 import math
+from string import ascii_letters, digits
+import random
+
+import time
 
 # Quantum-resistant hash function (SHA-3)
 def hmac_sha3(key, message):
     return hmac.new(key, message, sha3_256).digest()
 
-def combined_chaos_map(x: float, y: float, r: float = 3.9, a: float = 1.4, b: float = 0.3) -> Tuple[float, float]:
+def combined_chaos_map(x: float, y: float, z: float, r: float = 3.9, a: float = 1.4, b: float = 0.3) -> Tuple[float, float, float]:
     x_next = r * x * (1 - x)
     y_next = 1 - a * y**2 + b * x
-    return x_next % 1, y_next % 1
+    z_next = z * (1 - y) # Example equation; you can modify this
+    return x_next % 1, y_next % 1, z_next % 1
+
 
 secure_seed = secrets.randbits(128)
 rng = Generator(PCG64(secure_seed))
@@ -32,8 +38,13 @@ def lattice_keygen():
     private_key = s
     return public_key, private_key
 
+def generate_random_padding(length=2000):
+    characters = list(ascii_letters + digits)  # Convert to list
+    return ''.join(random.choice(characters) for _ in range(length))
 
 def logistic_lattice_chaos_encapsulation(message: str, public_key: tuple, shared_secret_key) -> Tuple[str, np.ndarray, bytes, float]:
+    padding = generate_random_padding()  # Generate random padding
+    message_with_padding = padding + message  # Concatenate with the original message
     A, b = public_key
     seed = secrets.randbits(128)
     rng = Generator(PCG64(seed))
@@ -41,11 +52,11 @@ def logistic_lattice_chaos_encapsulation(message: str, public_key: tuple, shared
     error_vector = sample_gaussian(A.shape[0])
     u = (A @ random_vector + error_vector) % q
     iv = rng.random()
-    x_initial, y_initial = iv, 0.5
+    x_initial, y_initial, z_initial = iv, rng.random(), rng.random()  # Three initial conditions
     encrypted_message = ""
-    for char in message:
-        x_initial, y_initial = combined_chaos_map(x_initial, y_initial)
-        key = ord(char) + int((x_initial + y_initial) * 256)
+    for char in message_with_padding:
+        x_initial, y_initial, z_initial = combined_chaos_map(x_initial, y_initial, z_initial)  # Three variables
+        key = ord(char) + int((x_initial + y_initial + z_initial) * 256)  # Combined with z
         encrypted_message += chr(int(key) % 256)
     mac = hmac_sha3(shared_secret_key, encrypted_message.encode('utf-8'))
     return encrypted_message, u, mac, iv
@@ -53,16 +64,16 @@ def logistic_lattice_chaos_encapsulation(message: str, public_key: tuple, shared
 def logistic_lattice_chaos_decapsulation(encrypted_message: str, u: np.ndarray, private_key, shared_secret_key, received_mac, iv: float) -> str:
     A, b = public_key
     v = (u - (A @ private_key)) % q
-    x_initial, y_initial = iv, 0.5
-    decrypted_message = ""
+    x_initial, y_initial, z_initial = iv, 0.5, 0.5  # Three initial conditions
+    decrypted_message_with_padding = ""
     for char in encrypted_message:
-        x_initial, y_initial = combined_chaos_map(x_initial, y_initial)
-        key = ord(char) - int((x_initial + y_initial) * 256)
-        decrypted_message += chr(int(key) % 256)
+        x_initial, y_initial, z_initial = combined_chaos_map(x_initial, y_initial, z_initial)  # Three variables
+        key = ord(char) - int((x_initial + y_initial + z_initial) * 256)  # Combined with z
+        decrypted_message_with_padding += chr(int(key) % 256)
     computed_mac = hmac_sha3(shared_secret_key, encrypted_message.encode('utf-8'))
     if computed_mac != received_mac:
         raise Exception("MAC verification failed")
-    return decrypted_message
+    return decrypted_message_with_padding[2000:]  # Remove the padding
 
 def calculate_differential(string1: str, string2: str) -> float:
     differences = sum(c1 != c2 for c1, c2 in zip(string1, string2))
@@ -79,10 +90,9 @@ def calculate_entropy(message):
     entropy = -sum((count / total_chars) * math.log2(count / total_chars) for count in frequencies.values())
     return entropy
 
-# Testing the code
-message1 = "heoodof waf f "
-message2 = "fewaefs wfewe  wfae"
-public_key, private_key = lattice_keygen()
+message1 = "fcvvv "
+message2 = "vvvv"
+public_key, private_key = lattice_keygen() 
 shared_secret_key = secrets.token_bytes(16)
 encrypted_message1, u1, mac1, iv1 = logistic_lattice_chaos_encapsulation(message1, public_key, shared_secret_key)
 encrypted_message2, _, _, _ = logistic_lattice_chaos_encapsulation(message2, public_key, shared_secret_key)
@@ -99,6 +109,26 @@ entropy_encrypted_message2 = calculate_entropy(encrypted_message2)
 key_length = len(shared_secret_key)
 possible_values = 256  # 2^8 possible values for each byte
 key_entropy = key_length * math.log2(possible_values)
+# Key Generation Timing
+start_time_keygen = time.time()
+public_key, private_key = lattice_keygen()
+end_time_keygen = time.time()
+keygen_duration = end_time_keygen - start_time_keygen
+
+# Encryption Timing
+start_time_enc = time.time()
+encrypted_message1, u1, mac1, iv1 = logistic_lattice_chaos_encapsulation(message1, public_key, shared_secret_key)
+end_time_enc = time.time()
+encryption_duration = end_time_enc - start_time_enc
+
+# Decryption Timing
+start_time_dec = time.time()
+decrypted_message1 = logistic_lattice_chaos_decapsulation(encrypted_message1, u1, private_key, shared_secret_key, mac1, iv1)
+end_time_dec = time.time()
+decryption_duration = end_time_dec - start_time_dec
+
+# Print Results
+
 
 # Results
 decryption_success = decrypted_message1 == message1
@@ -109,3 +139,6 @@ print("Entropy of private key:", entropy_private_key)
 print("Entropy of encrypted message 1:", entropy_encrypted_message1)
 print("Entropy of encrypted message 2:", entropy_encrypted_message2)
 print("Estimated key entropy:", key_entropy, "bits")
+print("Key Generation Time:", keygen_duration, "seconds")
+print("Encryption Time:", encryption_duration, "seconds")
+print("Decryption Time:", decryption_duration, "seconds")
